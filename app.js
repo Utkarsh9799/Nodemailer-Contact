df0c98abf8a867b3f3172192f0f0065c0325907d
Utkarsh Kumar <utkarsh9799@gmail.com>
2019-07-14 23:08:06 +0530

    Ethereal email smtp fix

--- app.js
+++ app.js
@@ -27,6 +27,55 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
+app.post('/send', (req,res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Name: ${req.bpdy.name}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone number: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+  
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'kaylee.kunze80@ethereal.email', // generated ethereal user
+        pass: 'gKYw5ub25xPTP4dTJH'  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Nodemailer Contact" <kaylee.kunze80@ethereal.email>', // sender address
+      to: 'utkarsh9799@gmail.com', // list of receivers
+      subject: 'Node Contact Request', // Subject line
+      text: 'Node mail incoming', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('index', () => {
+        alert('Mail sent successfully!');
+      });
+  });
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
